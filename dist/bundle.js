f20bfd5bad16c7d2ed22bc6fca8a3de4c9f4b198
Sergio Cortes <sergiocortessat@gmail.com>
2021-06-09 13:15:59 +0200

    Add webpack and initial phaser setup

--- dist/bundle.js
+++ dist/bundle.js
@@ -9,13 +9,13 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/index.js":
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
+/***/ "./src/js/customLogic/index.js":
+/*!*************************************!*\
+  !*** ./src/js/customLogic/index.js ***!
+  \*************************************/
 /***/ (() => {
 
-eval("\n\n//# sourceURL=webpack://JSCapstone-PlatformGame/./src/index.js?");
+eval("\n\n//# sourceURL=webpack://JSCapstone-PlatformGame/./src/js/customLogic/index.js?");
 
 /***/ })
 
@@ -26,7 +26,7 @@ eval("\n\n//# sourceURL=webpack://JSCapstone-PlatformGame/./src/index.js?");
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
 /******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	__webpack_modules__["./src/js/customLogic/index.js"]();
 /******/ 	
 /******/ })()
 ;
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: './src/js/customLogic/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
