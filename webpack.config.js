bea4a22384716867869350b5183086d134a6b0cf
Sergio Cortes <sergiocortessat@gmail.com>
2021-06-09 14:00:19 +0200

    Add phaser tutorial testing

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/js/customLogic/index.js',
+  entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
