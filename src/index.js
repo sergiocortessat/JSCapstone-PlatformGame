a01d49481ba0dd6b181c1372d1962e2c74a9a4f1
Sergio Cortes <sergiocortessat@gmail.com>
2021-06-10 10:44:12 +0200

    Add new assets to phaser, bootscene

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import './style/style.css';
-import 'phaser';
+import Phaser from 'phaser';
 import config from './phaser_js/config/config';
 import GameScene from './phaser_js/scenes/GameScene';
-
+import BootScene from './phaser_js/scenes/BootScene';
+import PreloaderScene from './phaser_js/scenes/PreloaderScene';
+import TitleScene from './phaser_js/scenes/TitleScene';
+import OptionsScene from './phaser_js/scenes/OptionsScene';
+import CreditsScene from './phaser_js/scenes/CreditsScene';
+ 
 class Game extends Phaser.Game {
-  constructor() {
+  constructor () {
     super(config);
+    this.scene.add('Boot', BootScene);
+    this.scene.add('Preloader', PreloaderScene);
+    this.scene.add('Title', TitleScene);
+    this.scene.add('Options', OptionsScene);
+    this.scene.add('Credits', CreditsScene);
     this.scene.add('Game', GameScene);
     this.scene.start('Game');
   }
 }
-
+ 
 window.game = new Game();
