02d9d19b216ca3d334e1a6770d73d5fd2a254f03
Sergio Cortes <sergiocortessat@gmail.com>
2021-06-10 19:22:20 +0200

    Add assets

---
+++ src/phaser_js/scenes/LeaderBoardScene.js
@@ -0,0 +1,33 @@
+import Phaser from 'phaser';
+// import Data from '../modules/data';
+// import Board from '../modules/scoreBoard';
+import config from '../config/config';
+
+export default class LeaderBoard extends Phaser.Scene {
+  constructor() {
+    super('LeaderBoard');
+  }
+
+  preload() {
+    this.load.html('leaderBoard', 'assets/board.html');
+  }
+
+  create() {
+    const self = this;
+
+    this.add.text(config.width / 3.3, 10, 'Leader Board', { color: 'white', fontSize: '32px ' });
+
+    const element = this.add.dom(400, 250).createFromCache('leaderBoard');
+
+    element.setPerspective(800);
+
+    element.addListener('click');
+
+    element.on('click', (event) => {
+      if (event.target.name === 'loginButton') {
+        self.scene.start('Title');
+      }
+    });
+    // Board.create();
+  }
+}
