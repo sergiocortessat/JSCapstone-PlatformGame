954c43d4ef3c3769404ba261cd852c9348c00dad
Sergio Cortes <sergiocortessat@gmail.com>
2021-06-14 13:00:10 +0200

    Add final testes for Model

--- src/__test__/Model.test.js
+++ src/__test__/Model.test.js
@@ -1,25 +1,32 @@
 import { test } from '@jest/globals';
 import Model from '../Model';
 
-describe('It testes Model phaser script', () => {
-
+describe('It testes Model for Phaser script', () => {
+  const mockModel = new (Model)();
   test('Test that background music does not paly immediately at the game starting', () => {
-    const mockModel = new (Model)();
     expect(mockModel.bgMusicPlaying).toBeFalsy();
   });
 
   test('Test Music to be enabled to accepts music', () => {
-    const mockModel = new (Model)();
     expect(mockModel.bgMusicPlaying).toBeFalsy();
   });
 
   test('Test Sound to be enabled to accepts music', () => {
-    const mockModel = new (Model)();
     expect(mockModel.bgMusicPlaying).toBeFalsy();
   });
 
   test('Turning on the background music', () => {
-    const musicModelObj = new Model();
-    expect(musicModelObj.bgMusicPlaying).toEqual(false);
+    mockModel.bgMusicPlaying = true;
+    expect(mockModel.bgMusicPlaying).not.toBe(false);
+  });
+
+  test('Changing the settings on game sound', () => {
+    mockModel.musicPlaying = true;
+    expect(mockModel.musicPlaying).not.toBe(false);
+  });
+
+  test('Changing the settings on game music', () => {
+    mockModel.soundPlaying = true;
+    expect(mockModel.soundPlaying).not.toBe(false);
   });
 });
