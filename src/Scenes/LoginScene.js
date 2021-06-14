e95a50671225fdfb5a68a70844c13e0207352115
Sergio Cortes <sergiocortessat@gmail.com>
2021-06-14 17:33:19 +0200

    Add linters fix

--- src/Scenes/LoginScene.js
+++ src/Scenes/LoginScene.js
@@ -22,7 +22,7 @@ export default class LoginScene extends Phaser.Scene {
           this.setVisible(false);
           text.setText(`Current Player: ${inputText.value}`);
           gameConfig.user = inputText.value;
-          window.game.scene.start('Options');
+          window.game.scene.start('Title');
         } else {
           this.scene.tweens.add({
             targets: text,
