222cac81939ef799b693016bc8cd267f59d44053
Sergio Cortes <sergiocortessat@gmail.com>
2021-06-14 12:49:10 +0200

    Add more models test

--- src/__test__/Model.test.js
+++ src/__test__/Model.test.js
@@ -1,4 +1,25 @@
 import { test } from '@jest/globals';
 import Model from '../Model';
 
-test('')
+describe('It testes Model phaser script', () => {
+
+  test('Test that background music does not paly immediately at the game starting', () => {
+    const mockModel = new (Model)();
+    expect(mockModel.bgMusicPlaying).toBeFalsy();
+  });
+
+  test('Test Music to be enabled to accepts music', () => {
+    const mockModel = new (Model)();
+    expect(mockModel.bgMusicPlaying).toBeFalsy();
+  });
+
+  test('Test Sound to be enabled to accepts music', () => {
+    const mockModel = new (Model)();
+    expect(mockModel.bgMusicPlaying).toBeFalsy();
+  });
+
+  test('Turning on the background music', () => {
+    const musicModelObj = new Model();
+    expect(musicModelObj.bgMusicPlaying).toEqual(false);
+  });
+});
