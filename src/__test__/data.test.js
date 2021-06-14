981477b6fd307891685fc00e96905d52c335abe9
Sergio Cortes <sergiocortessat@gmail.com>
2021-06-14 13:34:14 +0200

    Add data test

---
+++ src/__test__/data.test.js
@@ -0,0 +1,22 @@
+import { test } from '@jest/globals';
+import * as Score from '../modules/data';
+
+describe('LeaderBoard', () => {
+  test('Receiving information from API to populate table', () => {
+    Score.sendScore('Praz', 57).then((result) => {
+      expect(typeof result).toBe('JSON');
+    }).catch((err) => err);
+  });
+
+  test('it should send player name and score', () => {
+    Score.sendScore('Thapa', 62).then((result) => {
+      expect(result.result).toBe('Leaderboard score created correctly.');
+    }).catch((err) => err);
+  });
+
+  test('It should return the type of Score Getter', () => {
+    Score.showScore().then((result) => {
+      expect(result.result).toBe('Leaderboard score created correctly.');
+    }).catch((err) => err);
+  });
+});
