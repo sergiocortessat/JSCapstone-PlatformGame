8e7f1abbd662b6ebb746ab84e61a4d28be4ecd88
Sergio Cortes <sergiocortessat@gmail.com>
2021-06-14 12:34:18 +0200

    Add TitleScene test

--- src/__test__/TitleScene.test.js
+++ src/__test__/TitleScene.test.js
@@ -1,7 +1,7 @@
-import GameOverScene from '../Scenes/GameOverScene';
+import TitleScene from '../Scenes/TitleScene';
 
-jest.mock('../Scenes/GameOverScene');
+jest.mock('../Scenes/TitleScene');
 
 test('Testing the Game Over Scene', () => {
-  expect(new GameOverScene()).toBeInstanceOf(GameOverScene);
+  expect(new TitleScene()).toBeInstanceOf(TitleScene);
 });
