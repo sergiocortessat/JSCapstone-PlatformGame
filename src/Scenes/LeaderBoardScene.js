3840283942dc3d992d458b3bfbb68db48512a63b
Sergio Cortes <sergiocortessat@gmail.com>
2021-06-14 17:31:08 +0200

    Add linters fix

--- src/Scenes/LeaderBoardScene.js
+++ src/Scenes/LeaderBoardScene.js
@@ -1,35 +1,3 @@
-// import Data from '../modules/data';
-// import Board from '../modules/scoreBoard';
-
-// export default class LeaderBoard extends Phaser.Scene {
-//   constructor() {
-//     super('LeaderBoard');
-//   }
-
-//   preload() {
-//     this.load.html('leaderBoard', 'assets/board.html');
-//   }
-
-//   create() {
-//     const self = this;
-
-//     this.add.text(gameConfig.width / 2.5, 55, 'Leader Board', { color: 'white', fontSize: '32px ' });
-
-//     const element = this.add.dom(680, 250).createFromCache('leaderBoard');
-
-//     element.setPerspective(800);
-
-//     element.addListener('click');
-
-//     element.on('click', (event) => {
-//       if (event.target.name === 'loginButton') {
-//         self.scene.start('Title');
-//       }
-//     });
-//     Board.create();
-//   }
-// }
-
 import Phaser from 'phaser';
 import { showScore } from '../modules/data';
 import { gameConfig } from '../Config/config';
