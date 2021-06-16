48d8864eb468032ca181b9d12399c66c04bdad1c
Sergio Cortes <sergiocortessat@gmail.com>
2021-06-16 10:43:32 +0200

    Add negative testes for data scene

--- src/__test__/LeaderBoardScene.test.js
+++ src/__test__/LeaderBoardScene.test.js
@@ -2,6 +2,11 @@ import LeaderBoardScene from '../Scenes/LeaderBoardScene';
 
 jest.mock('../Scenes/LeaderBoardScene');
 
-test('Testing the LeaderBoard table Scene', () => {
-  expect(new LeaderBoardScene()).toBeInstanceOf(LeaderBoardScene);
+describe('It testes the Leader Board Scene', () => {
+  test('Testing the LeaderBoard table Scene', () => {
+    expect(new LeaderBoardScene()).toBeInstanceOf(LeaderBoardScene);
+  });
+  test('Testing the main LeaderBoard Scene does belong only to it object instance', () => {
+    expect(new (LeaderBoardScene)()).not.toBe(typeof Function);
+  });
 });
