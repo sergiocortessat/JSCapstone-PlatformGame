0be7fe20dad155764dbf2bb226e3aaf6ed68a5aa
Sergio Cortes <sergiocortessat@gmail.com>
2021-06-16 10:47:55 +0200

    Add negative tests to Model

--- src/__test__/Model.test.js
+++ src/__test__/Model.test.js
@@ -3,8 +3,12 @@ import Model from '../Model';
 
 describe('It testes Model for Phaser script', () => {
   const mockModel = new (Model)();
-  test('Test that background music does not paly immediately at the game starting', () => {
+  test('Test that background music does not play immediately at the game starting', () => {
     expect(mockModel.bgMusicPlaying).toBeFalsy();
+  });
+
+  test('Test that background music is not set to true', () => {
+    expect(mockModel.bgMusicPlaying).not.toBeTruthy();
   });
 
   test('Test Music to be enabled to accepts music', () => {
@@ -13,6 +17,10 @@ describe('It testes Model for Phaser script', () => {
 
   test('Test Sound to be enabled to accepts music', () => {
     expect(mockModel.bgMusicPlaying).toBeFalsy();
+  });
+
+  test('Test Sound not to be enabled', () => {
+    expect(mockModel.bgMusicPlaying).not.toBeTruthy();
   });
 
   test('Turning on the background music', () => {
@@ -28,5 +36,10 @@ describe('It testes Model for Phaser script', () => {
   test('Changing the settings on game music', () => {
     mockModel.soundPlaying = true;
     expect(mockModel.soundPlaying).not.toBe(false);
+  });
+
+  test('Not changing the settings on game music', () => {
+    mockModel.soundPlaying = true;
+    expect(mockModel.soundPlaying).toBe(true);
   });
 });
