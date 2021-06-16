7c33bfdc5e631ff4a64b2e2497585928ce8c5741
Sergio Cortes <sergiocortessat@gmail.com>
2021-06-16 10:43:41 +0200

    Add negative testes for credit scene

--- src/__test__/data.test.js
+++ src/__test__/data.test.js
@@ -1,12 +1,16 @@
 import { test } from '@jest/globals';
 import * as Score from '../modules/data';
 
-// jest.mock('../modules/data');
-
 describe('LeaderBoard', () => {
   test('Receiving information from API to populate table', () => {
     Score.sendScore('Praz', 57).then((result) => {
       expect(typeof result).toBe('JSON');
+    }).catch((err) => err);
+  });
+
+  test('Not receiving information from API to populate table', () => {
+    Score.sendScore('Praz', 57).then((result) => {
+      expect(typeof result).not.toBe('JSON');
     }).catch((err) => err);
   });
 
@@ -16,9 +20,21 @@ describe('LeaderBoard', () => {
     }).catch((err) => err);
   });
 
+  test('it should not send player name and score', () => {
+    Score.sendScore('Thapa', 62).then((result) => {
+      expect(result.result).not.toBe('Leaderboard score created correctly.');
+    }).catch((err) => err);
+  });
+
   test('It should return the type of Score Getter', () => {
     Score.showScore().then((result) => {
       expect(result.result).toBe('Leaderboard score created correctly.');
+    }).catch((err) => err);
+  });
+
+  test('It should not return the type of Score Getter', () => {
+    Score.showScore().then((result) => {
+      expect(result.result).not.toBe('Leaderboard score created correctly.');
     }).catch((err) => err);
   });
 });
